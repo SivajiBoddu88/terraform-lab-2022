eb9b721e7c6d34f20acbda36bac48a0a2b371401
SivajiBoddu88 <126242678+SivajiBoddu88@users.noreply.github.com>
2023-03-03 15:26:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f8ca728008ff5af4"
+  ami           = "ami-09ba48996007c8b50"
   instance_type = "t2.micro"
   availability_zone = "ap-south-1a"
   tags = {
-    Name = "sivaji_boddu"
+    Name = "sivaji_boddu_01"
   }
 }
